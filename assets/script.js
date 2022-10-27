e5d7de5ec21837d88047491c55442a707338a34a
Bailey Mejia <baileymejia28@gmail.com>
2022-10-26 22:06:06 -0700

    Added function for 5 day forecast will have to finish it

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ $("#city-name").text(momentDate); //Need to add container for HTML for date
 var citySearch = $("#city-lists");
 var searchBtn = document.getElementById("btn btn-outline");
 var currentForecast = document.querySelector("#The-weather");
-var weekForecast = document.getElementById("5-dayForecast");
+var weekForecast = document.querySelector(".forecast");
 var apiKey ='6fd2a7e7bfa6dd9234d5df15d716d7d3';
 var cityName = document.querySelector("#city-name");
 var cityList = [];
@@ -26,12 +26,12 @@ function getCityConditions() {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        // console.log(data);
         var {temp} = data.main;
         var place = data.name;
         var {description, icon} = data.weather[0];
         var {sunrise, sunset} =  data.sys;
-        var fahrenheit = (temp * 0.5)  / 5 + 32;
+        var fahrenheit = (temp * 0.9)  / 5 + 32;
         var sunrise = new Date(sunrise * 1000);
         var sunset = new Date(sunset * 1000);
         cityName.textContent = `${place}`;
@@ -45,8 +45,26 @@ getCityConditions();
 //-----------------------------5 Day forecast--------------------------------------------------------------------------------------------------------------------------------------------------------
 function getFiveDayForecast() {
     var town = "riverside"
-  var forecastWeather = "https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=" + apiKey;
-  console.log(forecastWeather);
+  var forecastWeather = "https://api.openweathermap.org/data/2.5/forecast?q="+ town + "&appid=" + apiKey + "&cnt=5";
+  fetch(forecastWeather)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var getArray = data.list;
+    for (i = 0; i < getArray.length; i++) {
+        console.log(getArray[i]);
+        //Finish array to be able to access the array elements***
+    }
+    var foreCastTemperture = data.list;
+        // foreCastTemperture= data.list;
+        // console.log(foreCastTemperture);
+        weekForecast.textContent = `${foreCastTemperture}`
+    
+
+  })
+  //  make this function like the daily forecast and see how to add it
 }
 getFiveDayForecast();
 
@@ -83,20 +101,9 @@ getFiveDayForecast();
 //forecasted conditions
 //read in unix date and return standard date
 
-//create card to display current weather information
 
 //set color for UV Index
 
 //create forecast cards
 
 //print daily forecast
-
-
-
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
